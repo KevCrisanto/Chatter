d42edcf735d6a86ca3965022f96d7892c3ec364f
Kevin <kevincrisanto@gmail.com>
2018-05-10 18:47:27 -0700

    Users are able to send messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { RESOURCE_CACHE_PROVIDER } from '@angular/platform-browser-dynamic';
+
+declare var window;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +20,19 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      window["ApiAIPlugin"].init(
+        {
+          clientAccessToken: "c66829bebb57406da609fb1bcebbb440",
+          lang: "en"
+        },
+        function (result) {
+          alert(result)
+        },
+        function (error) {
+          alert(error)
+        }
+      );
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var window;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,6 +10,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   messages: any[] = [];
+  text: string = "";
 
   constructor(public navCtrl: NavController) {
 
@@ -16,9 +19,28 @@ export class HomePage {
       sender: "api"
     })
 
+  }
+
+  sendText() {
+
+    let message = this.text;
+
     this.messages.push({
-      text: "Hello",
-      sender: "me"
+      text: this.text,
+      sender: 'me'
+    })
+
+    this.text = "";
+
+    window["ApiAIPlugin"].requestText({
+      query: message
+    }, (response) => {
+      this.messages.push({
+        text: response.result.fullfilment.speech,
+        sender: "api"
+      })
+    }, (error) => {
+      alert(JSON.stringify(error));
     })
   }
 
