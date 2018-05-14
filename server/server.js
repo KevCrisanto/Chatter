023c9a8f3350db8e44d121a269ab60b808567f36
Kevin <kevincrisanto@gmail.com>
2018-05-14 12:55:31 -0700

    AI can multiply, divide and subtract

--- server/server.js
+++ server/server.js
@@ -10,33 +10,65 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 app.post('/', function(req, res){
-    console.log(req.body);
+  console.log(req.body);
+  
+  if(req.body.queryResult.action == "checkVote"){
+    var age = req.body.queryResult.parameters.age;
+    console.log(age);
     
-    if(req.body.queryResult.action == "checkVote"){
-        var age = req.body.queryResult.parameters.age;
-        console.log(age);
-        
-        var response = "";
-        
-        if(age.amount >= 18){
-            response = "Yes";
-        } else {
-            response = "No";
-        }
-        
-        res.json({
-            "fulfillmentText": response
-        })
-    } else if(req.body.queryResult.action == "Add"){
-        var number1 = parseFloat(req.body.queryResult.parameters.number1);
-        var number2 = parseFloat(req.body.queryResult.parameters.number2);
-        var sum = number1 + number2;
-        var responseText = "The sum of " + number1 + " and " + number2 + " is " + sum;
-        
-        res.json({
-            "fulfillmentText": responseText
-        })
+    var response = "";
+    
+    if(age.amount >= 18){
+      response = "Yes";
+    } else {
+      response = "No";
     }
+    
+    res.json({
+      "fulfillmentText": response
+    })
+  } else if(req.body.queryResult.action == "Add"){
+    var number1 = parseFloat(req.body.queryResult.parameters.number1);
+    var number2 = parseFloat(req.body.queryResult.parameters.number2);
+    var sum = number1 + number2;
+    var responseText = "The sum of " + number1 + " and " + number2 + " is " + sum;
+    
+    res.json({
+      "fulfillmentText": responseText
+    });
+  } else if(req.body.queryResult.action == "Subtract"){
+    var number1 = parseFloat(req.body.queryResult.parameters.number1);
+    var number2 = parseFloat(req.body.queryResult.parameters.number2);
+    var difference = number1 - number2;
+    var responseText = "The difference between " + number1 + " and " + number2 + " is " + difference;
+    
+    res.json({
+      "fulfillmentText": responseText
+    });
+  } else if(req.body.queryResult.action == "Multiply"){
+    var number1 = parseFloat(req.body.queryResult.parameters.number1);
+    var number2 = parseFloat(req.body.queryResult.parameters.number2);
+    var product = number1 * number2;
+    var responseText = "The product of " + number1 + " and " + number2 + " is " + product;
+    
+    res.json({
+      "fulfillmentText": responseText
+    });
+  } else if(req.body.queryResult.action == "Divide"){
+    var number1 = parseFloat(req.body.queryResult.parameters.number1);
+    var number2 = parseFloat(req.body.queryResult.parameters.number2);
+    if (number1 != 0){
+      var quo = number2 / number1;
+      var responseText = number2 + " divided by " + number1 + " is " + quo;
+    } else{
+      var responseText = "You cannot divide by 0";
+    }
+
+    
+    res.json({
+      "fulfillmentText": responseText
+    });
+  }
 });
 
 app.listen(port, ip);
